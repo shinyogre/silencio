b16857d2bf791c912f9e06da5a0f842d78713ff1
glenn <glenn@cooper.localdomain>
2018-04-29 18:07:04 -0400

    initial setup

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
 --[[
-*create black bar, 2 squares, and 1 collectible
-*click and drag +  click and place
-*create snap-to-shape-edge behavior
+There is a key "behind" a wall.  It is yellow.  On the other side of the wall, there is a yellow block and a red block.  The yellow block cannot pass through the wall, but the red block can.  The red block cannot collect the yellow key.  The red block and the yellow block must be snapped together in a way that matches the orientation of the wall, then both blocks can pass through.
+
+An entity is an empty thing that has only a list of components.  e.g. a 'player' entity might contain a position, sprite, and control component.
+
+Components just contain variables.  No methods.  SYSTEMS handle methods for components.  A world correlates systems with components.
 
 ]]
 
@@ -16,7 +18,6 @@ function love.update(dt)
 end
 
 function love.draw()
-  lg.setBackgroundColor(1,1,1)
-  lg.setColor(0,0,0)
-  lg.rectangle('fill',450,0,64,720)
 end
+
+
